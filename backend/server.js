bffc1d1967c94ff81b907a80aad996971729e664
enzotiberghien <enzotiberghien28@gmail.com>
2023-05-10 14:51:08 +0200

    production

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,51 @@ app.post('/create-checkout-session', async (req, res) => {
 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
+    shipping_address_collection: {
+      allowed_countries: ['FR', 'SE'],
+    },
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          type: 'fixed_amount',
+          fixed_amount: {
+            amount: 0,
+            currency: 'eur',
+          },
+          display_name: 'Free shipping',
+          delivery_estimate: {
+            minimum: {
+              unit: 'business_day',
+              value: 5,
+            },
+            maximum: {
+              unit: 'business_day',
+              value: 7,
+            },
+          },
+        },
+      },
+      {
+        shipping_rate_data: {
+          type: 'fixed_amount',
+          fixed_amount: {
+            amount: 1500,
+            currency: 'eur',
+          },
+          display_name: 'Next day air',
+          delivery_estimate: {
+            minimum: {
+              unit: 'business_day',
+              value: 1,
+            },
+            maximum: {
+              unit: 'business_day',
+              value: 1,
+            },
+          },
+        },
+      },
+    ],
     line_items: items,
     mode: 'payment',
     success_url: 'https://yourwebsite.com/success',
