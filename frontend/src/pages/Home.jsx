d45f86300cc2b2b8db80d42deaa2842953ece67d
enzotiberghien <enzotiberghien28@gmail.com>
2023-05-10 17:28:59 +0200

    cookies

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,15 +7,30 @@ import Footer from '../components/Footer';
 
 
 const Home = () => {
+  const [showCookiePopup, setShowCookiePopup] = useState(true);
+
+  const handleAccept = () => {
+    // you should ideally also set the cookie in browser to remember the user's choice
+    setShowCookiePopup(false);
+  };
+
   return (
     <>
-      <Navbar></Navbar>
-      <Hero></Hero>
-      <AboutUs></AboutUs>
-      <Collection></Collection>
-      <Footer></Footer>
+      <Navbar />
+      <Hero />
+      <AboutUs />
+      <Collection />
+      {showCookiePopup && (
+        <div className="fixed bottom-0 left-0 right-0 bg-white p-4 shadow-md text-center text-sm">
+          <p>Nous utilisons des cookies pour optimiser votre expérience.</p>
+          <button className="mt-2 p-1 bg-blue-500 text-white rounded" onClick={handleAccept}>
+            J'accepte
+          </button>
+        </div>
+      )}
+      <Footer />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
