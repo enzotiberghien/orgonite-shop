a82574cd27835756b6d4f061311bfe01e7d94d57
enzotiberghien <enzotiberghien28@gmail.com>
2023-05-10 15:52:35 +0200

    test5

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -218,7 +218,7 @@ const Navbar = () => {
                             href="#"
                             onClick={(e)=> {
                               e.preventDefault()
-                              onClickCheckout()
+                              onClickCheckout(e)
                             }}
                             className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
                           >
