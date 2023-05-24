43a90d75d96e823688c7502cb6bdd1d54920fbe0
enzotiberghien <enzotiberghien28@gmail.com>
2023-05-24 11:00:38 +0200

    vendu

--- backend/Sanity/schemas/product.js
+++ backend/Sanity/schemas/product.js
@@ -9,5 +9,6 @@ export default {
     { name: 'imageSrc', title: 'Image Source', type: 'image' },
     { name: 'imageAlt', title: 'Image Alt', type: 'string' },
     { name: 'category', title: 'Category', type: 'string' },
+    { name: 'vendu', title: 'Vendu', type: 'boolean'}
     ],
 }
